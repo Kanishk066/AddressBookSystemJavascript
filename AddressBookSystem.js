847d022719002f5cc7f4ee26538495df1a0fa709
Kanishk066 <kanishksinghal06@gmail.com>
2023-06-04 19:26:54 +0530

    Added the code to create a new address book array and new contacts to it

--- AddressBookSystem.js
+++ AddressBookSystem.js
@@ -9,6 +9,75 @@ class Contact {
       this.setPhoneNumber(phoneNumber);
       this.setEmail(email);
     }
+
+    setFirstName(firstName) {
+        this.firstName = firstName;
+      }
+    
+      setLastName(lastName) {
+        this.lastName = lastName;
+      }
+    
+      setAddress(address) {
+        this.address = address;
+      }
+    
+      setCity(city) {
+        this.city = city;
+      }
+    
+      setState(state) {
+        this.state = state;
+      }
+    
+      setZip(zip) {
+        this.zip = zip;
+      }
+    
+      setPhoneNumber(phoneNumber) {
+        this.phoneNumber = phoneNumber;
+      }
+    
+      setEmail(email) {
+        this.email = email;
+      }
+    
+      // Getter methods for contact properties
+      getFirstName() {
+        return this.firstName;
+      }
+    
+      getLastName() {
+        return this.lastName;
+      }
+    
+      getAddress() {
+        return this.address;
+      }
+    
+      getCity() {
+        return this.city;
+      }
+    
+      getState() {
+        return this.state;
+      }
+    
+      getZip() {
+        return this.zip;
+      }
+    
+      getPhoneNumber() {
+        return this.phoneNumber;
+      }
+    
+      getEmail() {
+        return this.email;
+      }
+    
+      toString() {
+        return `${this.firstName} ${this.lastName}\nAddress: ${this.address}\nCity: ${this.city}\nState: ${this.state}\nZip: ${this.zip}\nPhone: ${this.phoneNumber}\nEmail: ${this.email}\n`;
+      }
   
     setFirstName(firstName) {
       const nameRegex = /^[A-Z][a-zA-Z]{2,}$/;
@@ -105,39 +174,67 @@ class Contact {
     }
   }
   
-  // Creating an AddressBook instance
-  const addressBook = new AddressBook();
+  // Creating a new Address Book array
+  const addressBooks = [];
   
-  // Adding contacts
+  // Adding contacts to the first Address Book
+  const addressBook1 = new AddressBook();
   try {
-    const contact1 =  new Contact(
-        "John",
-        "Doe",
-        "123 Main St",
-        "New York",
-        "NY",
-        "12345",
-        "1234567890",
-        "john.doe@example.com"
-      );
-      addressBook.addContact(contact1);
-    
-      const contact2 = new Contact(
-        "Jane",
-        "Smith",
-        "456 Elm St",
-        "Los Angeles",
-        "CA",
-        "98765",
-        "9876543210",
-        "jane.smith@example.com"
-      );
-      addressBook.addContact(contact2);
-    
-      // Displaying contacts
-      addressBook.displayContacts();
-    } catch (error) {
-      console.log("Error:", error.message);
-    }
+    const contact1 = new Contact(
+      "John",
+      "Doe",
+      "123 Main St",
+      "New York",
+      "NY",
+      "12345",
+      "1234567890",
+      "john.doe@example.com"
+    );
+    addressBook1.addContact(contact1);
+  
+    const contact2 = new Contact(
+      "Jane",
+      "Smith",
+      "456 Elm St",
+      "Los Angeles",
+      "CA",
+      "98765",
+      "9876543210",
+      "jane.smith@example.com"
+    );
+    addressBook1.addContact(contact2);
+  
+    // Adding addressBook1 to the addressBooks array
+    addressBooks.push(addressBook1);
+  } catch (error) {
+    console.log("Error:", error.message);
+  }
+  
+  // Adding contacts to the second Address Book
+  const addressBook2 = new AddressBook();
+  try {
+    const contact3 = new Contact(
+      "Alice",
+      "Johnson",
+      "789 Oak St",
+      "Chicago",
+      "IL",
+      "54321",
+      "9876543210",
+      "alice.johnson@example.com"
+    );
+    addressBook2.addContact(contact3);
+  
+    // Adding addressBook2 to the addressBooks array
+    addressBooks.push(addressBook2);
+  } catch (error) {
+    console.log("Error:", error.message);
+  }
+  
+  // Displaying all contacts in each Address Book
+  for (let i = 0; i < addressBooks.length; i++) {
+    console.log("Address Book " + (i + 1) + ":");
+    addressBooks[i].displayContacts();
+  }
   
   
